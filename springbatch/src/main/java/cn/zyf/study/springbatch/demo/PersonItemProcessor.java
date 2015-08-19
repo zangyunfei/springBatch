a4e33f4e68c6016ae2f8a2b3fa6dba5035a664fc
zangyunfei <zangyunfei@ucredit.com>
2015-08-19 16:22:36 +0800

    add

---
+++ springbatch/src/main/java/cn/zyf/study/springbatch/demo/PersonItemProcessor.java
@@ -0,0 +1,24 @@
+package cn.zyf.study.springbatch.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.ItemProcessor;
+
+public class PersonItemProcessor implements ItemProcessor<Person, Person> {
+
+    private static final Logger log = LoggerFactory
+        .getLogger(PersonItemProcessor.class);
+
+    public Person process(final Person person) {
+        final String firstName = person.getFirstName().toUpperCase();
+        final String lastName = person.getLastName().toUpperCase();
+
+        final Person transformedPerson = new Person(firstName, lastName);
+
+        PersonItemProcessor.log.info("Converting (" + person + ") into ("
+            + transformedPerson + ")");
+
+        return transformedPerson;
+    }
+
+}
